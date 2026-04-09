842d1a51c32b3690e48668aada11fcf844ce8f9f
Viktor Korobov <viktor.korobov@korobov-solutions.de>
2026-04-09 16:58:35 +0200

    refactor program to showcase polymorphism in action

--- Program.cs
+++ Program.cs
@@ -1,28 +1,13 @@
-﻿var sparkasse = Bank.Create("Sparkasse");
+﻿var someAccount = Account.Create("123456789012345678", 1000);
+var someDebitAccount = DebitAccount.Create("123456789012345678", 500, 500);
+var someSavingAccount = SavingAccount.Create("123456789012345678", 0.05, 900);
 
-var postbank = Bank.Create("Postbank");
+someSavingAccount.ApplyInterest(3);
 
-var account_1 = Account.Create("1001 46464461 1564"); 
-var account_2 = DebitAccount.Create("4546 00000000 4567", 1000);
+var accounts = new List<Account> { someAccount, someDebitAccount, someSavingAccount };
 
-var klaus = Client.Create("    kLaUs Günter  ", "Möritz");
-
-klaus.AddAccount(account_1);
-klaus.AddAccount(account_2);
-sparkasse.AddAccount(account_1);
-sparkasse.AddClient(klaus);
-
-var claudia = Client.Create("Claudia", "Möritz", klaus.Accounts);
-
-account_1.Deposit(100);
-klaus.Accounts[0].Deposit(100);
-claudia.Accounts[0].Deposit(100);
-klaus.Accounts[1].Withdraw(500);
-Console.WriteLine(klaus.Accounts[0].Balance);
-Console.WriteLine(claudia.Accounts[0].Balance);
-Console.WriteLine(sparkasse.Accounts[0].Balance);
-Console.WriteLine(sparkasse.Clients[0].Accounts[0].Balance);
-
-
-Console.WriteLine(klaus.FirstName);
-Console.WriteLine(klaus.Accounts[1].Balance);
+foreach (var account in accounts)
+{
+    account.Withdraw(1000);
+    Console.WriteLine($"Account number: {account.Num}, Balance: {account.Balance}");
+}
